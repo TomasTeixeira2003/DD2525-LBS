ea08c4c5b0e0b7bf08179674bbbb529ba9f6e4f5
EvaSSchreyer <me@example.com>
2025-05-07 14:53:26 +0200

    Solution for the first part of Task1 -- missing solution to overcome inter and intra app sniffing

--- Lab3/Intent Sniffing/IntentSniffer-Template/app/src/main/java/tcs/lbs/intentsniffer/MainActivity.java
+++ Lab3/Intent Sniffing/IntentSniffer-Template/app/src/main/java/tcs/lbs/intentsniffer/MainActivity.java
@@ -2,12 +2,20 @@ package tcs.lbs.intentsniffer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.location.Location;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import android.content.BroadcastReceiver;
 
 public class MainActivity extends AppCompatActivity
 {
     TextView LatitudeTextView, LongitudeTextView;
+    //BroadcastActivityReceiver mReceive;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,8 +27,35 @@ public class MainActivity extends AppCompatActivity
         LongitudeTextView = findViewById(R.id.LongitudeTextView);
 
 
-        // TODO Define a broadcast receiver class
-        // TODO Register to receive broadcast messages from ForegroundLocationService of the LocationApp
-        // TODO Extract Location coordinates from the broadcast massage and show them in LatitudeTextView and LongitudeTextView
+
+        // Register to receive broadcast messages from ForegroundLocationService of the LocationApp
+        // Register MainActivityReceiver class to listen to broadcasts from ForegroundLocationService
+        IntentFilter filter = new IntentFilter();
+
+        //for intra-app communication capturing
+        filter.addAction("tcs.lbs.weather_app.WeatherBroadcastReceiver");
+
+        //for inter-app communication capturing
+        //filter.addAction("tcs.lbs.locationapp.MainActivityReceiver"); //has to be this because of locationApp->ForegroundLocationService.java line 49
+
+        registerReceiver(new BroadcastActivityReceiver(), filter);
+    }
+
+    // Define a broadcast receiver class
+    public class BroadcastActivityReceiver extends BroadcastReceiver
+    {
+        // If a broadcast intent is received, this method will be invoked.
+        @Override
+        public void onReceive(Context context, Intent intent)
+        {
+            Location location = intent.getParcelableExtra("Location");
+            if (location != null)
+            {
+                // Extract Location coordinates from the broadcast massage and show them in LatitudeTextView and LongitudeTextView
+                LatitudeTextView.setText("Latitude: " + location.getLatitude());
+                LongitudeTextView.setText("Longitude: " + location.getLongitude());
+
+            }
+        }
     }
 }
